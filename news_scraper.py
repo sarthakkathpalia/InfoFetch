b660ee9d1bc28e4b49ded28afc4fc3ba65c9d9cd
sarthakkathpalia <sarthakkathpalia@gmail.com>
2023-05-30 09:17:45 +0530

    initial commit

---
+++ news_scraper.py
@@ -0,0 +1,59 @@
+from dotenv import load_dotenv
+import os 
+load_dotenv('.env')
+
+import requests 
+
+from bs4 import BeautifulSoup 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import datetime
+
+now = datetime.datetime.now()
+
+content = ''
+
+def extract_news(url):
+    print("Extracting Hacker News Stories...")
+    cnt = ''
+    cnt +=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup = BeautifulSoup(content,'html.parser')
+    for i,tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+ '<a href="' + tag.a.get('href') + '">' + tag.text + '</a>' + "\n" + '<br>') if tag.text!='More' else '')
+    return(cnt)
+cnt = extract_news('https://news.ycombinator.com/')
+content += cnt
+content += ('<br>------<br>')
+content +=('<br><br>End of Message')
+
+print('Composing Email...')
+
+SERVER = 'smtp.gmail.com' 
+PORT = 587  
+FROM:str =  os.getenv('USERNAME')
+TO = [''] 
+PASS:str = os.getenv('PASSWORD') 
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'Top News Stories HN [Automated Email]' + ' ' + str(now.day) + '-' + str(now.month) + '-' + str(now.year)
+msg['From'] = FROM
+msg['To'] = ', '.join(TO)
+
+msg.attach(MIMEText(content, 'html'))
+
+print('Initiating Server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM, PASS)
+server.sendmail(FROM, TO, msg.as_string())
+
+print('Email Sent...')
+
+server.quit()
